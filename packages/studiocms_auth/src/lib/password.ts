2ce28f7b91172d7422d8bc0cbd132150af2c18a2
Adam Matthiesen <amatthiesen@outlook.com>
2024-11-01 21:13:56 -0700

    test

--- packages/studiocms_auth/src/lib/password.ts
+++ packages/studiocms_auth/src/lib/password.ts
@@ -1,15 +1,16 @@
 import { checkIfUnsafe } from '@matthiesenxyz/integration-utils/securityUtils';
 import { sha1 } from '@oslojs/crypto/sha1';
 import { encodeHexLowerCase } from '@oslojs/encoding';
-import * as argon2 from 'argon2';
+import bcrypt from 'bcryptjs';
 
 export async function hashPassword(password: string): Promise<string> {
-	const hash = await argon2.hash(password);
-	return hash;
+	const hashedPassword = await bcrypt.hash(password, 10);
+	return hashedPassword;
 }
 
 export async function verifyPasswordHash(hash: string, password: string): Promise<boolean> {
-	return await argon2.verify(hash, password);
+	const passwordMatch = await bcrypt.compare(password, hash);
+	return passwordMatch;
 }
 
 export async function verifyPasswordStrength(password: string): Promise<boolean> {
