fd7c78dc7d55d72ae74cff8db5b8a3a3d4d5e1e3
Adam Matthiesen <amatthiesen@outlook.com>
2024-11-01 20:49:47 -0700

    test

--- packages/studiocms_auth/src/lib/password.ts
+++ packages/studiocms_auth/src/lib/password.ts
@@ -1,19 +1,15 @@
 import { checkIfUnsafe } from '@matthiesenxyz/integration-utils/securityUtils';
-import { hash, verify } from '@node-rs/argon2';
 import { sha1 } from '@oslojs/crypto/sha1';
 import { encodeHexLowerCase } from '@oslojs/encoding';
+import * as argon2 from 'argon2';
 
 export async function hashPassword(password: string): Promise<string> {
-	return await hash(password, {
-		memoryCost: 19456,
-		timeCost: 2,
-		parallelism: 1,
-		outputLen: 32,
-	});
+	const hash = await argon2.hash(password);
+	return hash;
 }
 
 export async function verifyPasswordHash(hash: string, password: string): Promise<boolean> {
-	return await verify(hash, password);
+	return await argon2.verify(hash, password);
 }
 
 export async function verifyPasswordStrength(password: string): Promise<boolean> {
