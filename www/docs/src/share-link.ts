ce23b086b0b7ac9158e42732b27750f64d5fd630
Adam Matthiesen <amatthiesen@outlook.com>
2024-11-27 07:00:10 -0800

    Update Turso sponsor link and add StudioCMS coupon code (#389)

--- www/docs/src/share-link.ts
+++ www/docs/src/share-link.ts
@@ -4,15 +4,10 @@ export { default as SponsorLink } from './util/SponsorLink.astro';
 export const sponsors = {
 	turso: {
 		docs: {
-			sidebarSponsorLink: 'https://turso.tech',
+			sidebarSponsorLink: 'https://tur.so/studiocms',
 			installCLILink: 'https://docs.turso.tech/cli/installation',
 			loginsignupLink: 'https://docs.turso.tech/cli/authentication',
 		},
-		web: {
-			sponsorLink: 'https://turso.tech',
-		},
-		// TODO: Add Turso specific links when they are provided to us.
-		// Also, switch to utilizing these new links within the websites.
 	},
 };
 
