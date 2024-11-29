80482e9a78e146ca814e086cf4b94a05b7073e8f
Adam Matthiesen <amatthiesen@outlook.com>
2024-11-29 00:58:03 -0800

    Add i18n to Docs (and Lunaria) (#386)
    * Update renderer configuration and sponsor title localization
    
    * add lunaria workflow
    
    * Update lunaria.config.json with GitHub hosting information
    
    * Update language options in astro.config.mts
    
    * test lunaria starlight plugin and fix typedoc types
    
    * remove lunaria plugin
    
    * upgrade to lunaria v1
    
    * Remove lunaria:preview script from package.json
    
    * Add cache directory for Lunaria
    
    * remove lunaria cache as it didnt help
    
    * test
    
    * add build action
    
    * test
    
    * Refactor build workflow to use PNPM for dependency management
    
    * Refactor lunaria.config.ts to exclude typedoc files from the build
    
    * Refactor lunaria.config.ts to exclude tracking configuration
    
    * update lunaria and docs config
    
    * update lunaria workflows
    
    * remove Astro's custom styling
    
    * Refactor lunaria.config.ts to update branch configuration
    
    * Refactor lunaria.config.ts to update branch configuration
    
    * Update package.json and pnpm-lock.yaml to add @actions/core dependency and add lunaria report bot functionality
    
    * Refactor lunaria/components.ts to fix typo in translation complete message

--- www/docs/src/content/config.ts
+++ www/docs/src/content/config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, reference } from 'astro:content';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 import { z } from 'astro/zod';
 
 const packageCatalogSchema = z.object({
@@ -18,6 +18,7 @@ const packageCatalogSchema = z.object({
 
 const baseSchema = z.object({
 	type: z.literal('base').optional().default('base'),
+	i18nReady: z.boolean().optional().default(false),
 });
 
 const integrationSchema = baseSchema.extend({
@@ -32,9 +33,32 @@ const redirectSchema = baseSchema.extend({
 
 const docsCollectionSchema = z.union([baseSchema, integrationSchema, redirectSchema]);
 
+const customTranslationsSchema = z.object({
+	'site-title.labels.docs': z.string().optional(),
+	'site-title.labels.main-site': z.string().optional(),
+	'site-title.labels.live-demo': z.string().optional(),
+	'sponsors.sponsoredby': z.string().optional(),
+	'package-catalog.readmore.start': z.string().optional(),
+	'package-catalog.readmore.end': z.string().optional(),
+	'integration-labels.changelog': z.string().optional(),
+	'contributors.core-packages': z.string().optional(),
+	'contributors.ui-library': z.string().optional(),
+	'contributors.devapps': z.string().optional(),
+	'contributors.plugins': z.string().optional(),
+	'contributors.documentation': z.string().optional(),
+	'contributors.website': z.string().optional(),
+	'contributors.bots': z.string().optional(),
+});
+
 export const collections = {
 	docs: defineCollection({
 		schema: docsSchema({ extend: docsCollectionSchema }),
+	}),
+	i18n: defineCollection({
+		type: 'data',
+		schema: i18nSchema({
+			extend: customTranslationsSchema,
+		}),
 	}),
 	'package-catalog': defineCollection({
 		type: 'data',
