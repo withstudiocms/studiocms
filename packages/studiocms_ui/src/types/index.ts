ae6ea116f71fb1275232717b6035f70f6b96c3f1
Louis Escher <louisescher@proton.me>
2024-12-01 16:11:06 +0100

    Update toaster to allow for innerHTML in descriptions

--- packages/studiocms_ui/src/types/index.ts
+++ packages/studiocms_ui/src/types/index.ts
@@ -1,5 +1,8 @@
 export type ToastProps = {
 	title: string;
+	/**
+	 * This will get passed to the component as unsanitized HTML. DO NOT PUT USER-GENERATED CONTENT HERE!
+	 */
 	description?: string;
 	type: 'success' | 'warning' | 'danger' | 'info';
 	duration?: number;
