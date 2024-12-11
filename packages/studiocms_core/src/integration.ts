da32c3493dfcf00d1b4a2c53f6135da77df15720
Adam Matthiesen <amatthiesen@outlook.com>
2024-12-11 01:59:49 -0800

    Refactor StudioCMS SDK to include SDK stubs and update JSDocs

--- packages/studiocms_core/src/integration.ts
+++ packages/studiocms_core/src/integration.ts
@@ -1,11 +1,12 @@
 import { integrationLogger } from '@matthiesenxyz/integration-utils/astroUtils';
 import type { InjectedType } from 'astro';
-import { createResolver, defineIntegration } from 'astro-integration-kit';
+import { addVirtualImports, createResolver, defineIntegration } from 'astro-integration-kit';
 import { version } from '../package.json';
 import { name } from '../package.json';
 import { StudioCMSOptionsSchema as optionsSchema } from './schemas';
 import { CoreStrings } from './strings';
 import { i18nDTSOutput } from './stubs/i18n-dts';
+import { sdkDtsFile } from './stubs/sdk';
 import { coreVirtualModuleGeneration } from './utils/coreVirtualModules';
 
 export default defineIntegration({
@@ -14,6 +15,8 @@ export default defineIntegration({
 	setup({ name, options }) {
 		// Declaration for Core DTS File
 		let coreDtsFile: InjectedType;
+
+		const { resolve } = createResolver(import.meta.url);
 
 		return {
 			hooks: {
@@ -44,6 +47,40 @@ export default defineIntegration({
 						},
 					});
 
+					addVirtualImports(params, {
+						name,
+						imports: {
+							'studiocms:sdk': `
+								import studioCMS_SDK from '${resolve('./sdk-utils/index.ts')}';
+								export default studioCMS_SDK;
+							`,
+							'studiocms:sdk/get': `
+								import studioCMS_SDK_GET from '${resolve('./sdk-utils/get/index.ts')}';
+								export default studioCMS_SDK_GET;
+							`,
+							'studiocms:sdk/post': `
+								import studioCMS_SDK_POST from '${resolve('./sdk-utils/post/index.ts')}';
+								export default studioCMS_SDK_POST;
+							`,
+							'studiocms:sdk/update': `
+								import studioCMS_SDK_UPDATE from '${resolve('./sdk-utils/update/index.ts')}';
+								export default studioCMS_SDK_UPDATE;
+							`,
+							'studiocms:sdk/delete': `
+								import studioCMS_SDK_DELETE from '${resolve('./sdk-utils/delete/index.ts')}';
+								export default studioCMS_SDK_DELETE;
+							`,
+							'studiocms:sdk/auth': `
+								import studioCMS_SDK_AUTH from '${resolve('./sdk-utils/auth/index.ts')}';
+								export default studioCMS_SDK_AUTH;
+							`,
+							'studiocms:sdk/types': `
+								import type { STUDIOCMS_SDK } from '${resolve('./sdk-utils/types.ts')}';
+								export type { STUDIOCMS_SDK };
+							`,
+						},
+					});
+
 					// Set the DTS File
 					coreDtsFile = dtsFileOutput;
 				},
@@ -51,6 +88,7 @@ export default defineIntegration({
 					// Inject the DTS File
 					injectTypes(coreDtsFile);
 					injectTypes(i18nDTSOutput);
+					injectTypes(sdkDtsFile);
 				},
 			},
 		};
