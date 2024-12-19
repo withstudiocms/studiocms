f5fa569929560286b87f7944d4d3ffa0227b037e
Adam Matthiesen <amatthiesen@outlook.com>
2024-12-19 07:17:28 -0800

    Refactor VersionCheck component: Update client routable function and import paths for SDK utility functions

---
+++ packages/studiocms_dashboard/src/components/component-scripts/makeClientRoutable.ts
@@ -0,0 +1,7 @@
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function makeClientRoutable(func: any) {
+	func();
+	document.addEventListener('astro:page-load', () => {
+		func();
+	});
+}
