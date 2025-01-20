d84d6180128bd14028c6559127a2804b53add5ae
Adam Matthiesen <amatthiesen@outlook.com>
2025-01-19 21:01:34 -0800

    Add TinyMDE component and refactor Edit.astro to use shared options

---
+++ packages/studiocms_dashboard/src/components/islands/content-mgmt/shared.ts
@@ -0,0 +1,38 @@
+import pluginsList from 'studiocms:plugins';
+import { studioCMS_SDK_Cache } from 'studiocms:sdk/cache';
+
+interface PluginListItem {
+	label: string;
+	value: string;
+}
+
+const { data: folderList } = await studioCMS_SDK_Cache.GET.folderList();
+
+const parentFolders = folderList.map(({ id: value, name: label }) => ({ value, label }));
+
+// EXPORTS
+
+export const parentFolderOptions = [{ value: 'null', label: 'None' }, ...parentFolders];
+
+export const pageTypeOptions = pluginsList.flatMap(({ pageTypes }) => {
+	const pageTypeOutput: PluginListItem[] = [];
+
+	if (!pageTypes) {
+		return pageTypeOutput;
+	}
+
+	for (const { label, identifier } of pageTypes) {
+		pageTypeOutput.push({ label, value: identifier });
+	}
+
+	return pageTypeOutput;
+});
+
+export const trueFalse = [
+	{ label: 'Yes', value: 'true' },
+	{ label: 'No', value: 'false' },
+];
+
+export const categoriesOptions = [{ label: 'None', value: 'null' }];
+
+export const tagsOptions = [{ label: 'None', value: 'null' }];
