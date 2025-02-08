10375b37dcb68815f3b7119f47e0516635df43d8
Adam Matthiesen <amatthiesen@outlook.com>
2025-02-08 01:46:03 -0800

    Remove commented-out code for StudioCMS Renderers integration

--- packages/studiocms_renderers/src/index.ts
+++ packages/studiocms_renderers/src/index.ts
@@ -103,69 +103,6 @@ export const studioCMSRenderers = defineIntegration({
 	},
 });
 
-// /**
-//  * **StudioCMS Renderers Integration**
-//  *
-//  * @param options StudioCMS Renderer Configuration
-//  * @returns AstroIntegration
-//  *
-//  * @see [StudioCMS Docs](https://docs.studiocms.dev) for more information on how to use StudioCMS.
-//  */
-// export function studioCMSRenderers(
-// 	options: StudioCMSRendererConfig,
-// 	verbose?: boolean
-// ): AstroIntegration {
-// 	const { resolve } = createResolver(import.meta.url);
-// 	const RendererComponent = resolve('../components/Renderer.js');
-
-// 	return {
-// 		name: pkgName,
-// 		hooks: {
-// 			'astro:config:setup': (params) => {
-// 				// Destructure the params
-// 				const { logger, config } = params;
-
-// 				// Log that Setup is Starting
-// 				integrationLogger(
-// 					{ logger, logLevel: 'info', verbose },
-// 					'Setting up StudioCMS Renderer...'
-// 				);
-// 				// Setup the runtime logger
-// 				runtimeLogger(params, { name: 'studiocms-renderer' });
-
-// 				// Add Virtual Imports
-// 				addVirtualImports(params, {
-// 					name: pkgName,
-// 					imports: {
-// 						'studiocms:renderer': `export { default as StudioCMSRenderer } from '${RendererComponent}';`,
-// 						'studiocms:renderer/config': `export default ${JSON.stringify(options)}`,
-// 						'studiocms:renderer/astroMarkdownConfig': `export default ${JSON.stringify(config.markdown)}`,
-// 						'studiocms:renderer/current': `
-// 						export * from '${resolve('./lib/contentRenderer.js')}';
-// 						import contentRenderer from '${resolve('./lib/contentRenderer.js')}';
-// 						export default contentRenderer;
-// 						`,
-// 					},
-// 				});
-// 				integrationLogger(
-// 					{ logger, logLevel: 'info', verbose },
-// 					'StudioCMS Renderer Virtual Imports Added...'
-// 				);
-// 			},
-// 			'astro:config:done': ({ injectTypes }) => {
-// 				// Inject Types for Renderer
-// 				injectTypes(rendererDTS);
-
-// 				// Inject Types for Renderer Config
-// 				injectTypes(rendererConfigDTS);
-
-// 				// Inject Types for Astro Markdown Config
-// 				injectTypes(rendererMarkdownConfigDTS);
-// 			},
-// 		},
-// 	};
-// }
-
 export default studioCMSRenderers;
 
 export type { StudioCMSRendererConfig };
