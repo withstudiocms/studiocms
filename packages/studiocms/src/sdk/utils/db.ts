085666f9e397dcb020c17f96916f4950d827ff7c
Adam Matthiesen <amatthiesen@outlook.com>
2025-02-12 02:56:43 -0800

    Refactor database connection setup to use updated drizzle-orm syntax for improved clarity and maintainability

--- packages/studiocms/src/sdk/utils/db.ts
+++ packages/studiocms/src/sdk/utils/db.ts
@@ -1,17 +1,22 @@
 import { dbSecret, dbUrl } from 'virtual:studiocms/sdk/env';
-import { createClient } from '@libsql/client';
-import { drizzle } from 'drizzle-orm/libsql';
+// import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql/node';
 
-const client = createClient({
-	url: dbUrl,
-	authToken: dbSecret,
-});
+// const client = createClient({
+// 	url: dbUrl,
+// 	authToken: dbSecret,
+// });
 
 /**
  * Returns a new Drizzle libSQL connection.
  */
 export const useDB = () => {
-	const connection = drizzle(client);
+	const connection = drizzle({
+		connection: {
+			url: dbUrl,
+			authToken: dbSecret,
+		},
+	});
 
 	return connection;
 };
