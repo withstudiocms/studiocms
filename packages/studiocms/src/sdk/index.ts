62406bfb141e2aa63432f75b07caf63690ad8b8f
Adam Matthiesen <amatthiesen@outlook.com>
2025-02-12 11:17:40 -0800

    Remove unused imports and clean up index.ts in the SDK module

--- packages/studiocms/src/sdk/index.ts
+++ packages/studiocms/src/sdk/index.ts
@@ -1,17 +1,7 @@
-// import { and, asc, db, desc, eq } from 'astro:db';
-// import { getSecret } from 'astro:env/server';
-import SDK from './StudioCMSSDK.js';
 import { studiocmsSDKCore } from './core.js';
-// import type { STUDIOCMS_SDK } from './types/index.js';
-
-// const env = {
-// 	CMS_ENCRYPTION_KEY: getSecret('CMS_ENCRYPTION_KEY'),
-// };
 
 const sdkCore = studiocmsSDKCore();
 
 export const studioCMS_SDK = sdkCore;
 
 export default studioCMS_SDK;
-
-export { SDK as uninitializedStudioCMSSDK };
