b165c9e2afe890539c7b77f8b495e01b06817392
Adam Matthiesen <amatthiesen@outlook.com>
2025-02-12 15:37:42 -0800

    Remove unused components and configuration files from the dashboard

--- archive/dashboard/components/component-scripts/makeClientRoutable.ts
+++
@@ -1,7 +0,0 @@
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export function makeClientRoutable(func: any) {
-	func;
-	document.addEventListener('astro:page-load', () => {
-		func;
-	});
-}
