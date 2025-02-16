c01966d08aacedca5f146c33be9e85fce19aa27f
Adam Matthiesen <amatthiesen@outlook.com>
2025-02-16 04:44:27 -0800

    Implement user retrieval and creation endpoints with permission checks and validation

--- packages/studiocms/src/routes/rest/v1/users/[id].ts
+++ packages/studiocms/src/routes/rest/v1/users/[id].ts
@@ -1,11 +1,89 @@
-import type { APIRoute } from 'astro';
+import { verifyUserPermissionLevel } from 'studiocms:auth/lib/user';
+import studioCMS_SDK from 'studiocms:sdk';
+import type { APIContext, APIRoute } from 'astro';
+import { simpleResponse } from '../../../../utils/simpleResponse.js';
+import { verifyAuthToken } from '../../utils/auth-token.js';
+
+export const GET: APIRoute = async (context: APIContext) => {
+	const user = await verifyAuthToken(context);
+
+	if (user instanceof Response) {
+		return user;
+	}
+
+	const { rank } = user;
+
+	if (rank !== 'owner' && rank !== 'admin') {
+		return simpleResponse(401, 'Unauthorized');
+	}
+
+	const { id } = context.params;
+
+	if (!id) {
+		return simpleResponse(400, 'Invalid form data, id is required');
+	}
+
+	const existingUser = await studioCMS_SDK.GET.databaseEntry.users.byId(id);
+
+	if (!existingUser) {
+		return simpleResponse(400, 'User not found');
+	}
+
+	const { avatar, createdAt, email, name, permissionsData, updatedAt, url, username } =
+		existingUser;
+
+	const existingUserRank = (permissionsData?.rank ?? 'admin') as
+		| 'visitor'
+		| 'editor'
+		| 'admin'
+		| 'owner'
+		| 'unknown';
+
+	const data = {
+		avatar,
+		createdAt,
+		email,
+		id,
+		name,
+		rank: existingUserRank,
+		updatedAt,
+		url,
+		username,
+	};
+
+	const loggedInUser = (await studioCMS_SDK.GET.databaseTable.users()).find(
+		(user) => user.id === id
+	);
+
+	if (!loggedInUser || loggedInUser === undefined) {
+		return simpleResponse(400, 'User Error');
+	}
+
+	const permissionLevelInput = {
+		isLoggedIn: true,
+		user: loggedInUser,
+		permissionLevel: rank as 'visitor' | 'editor' | 'admin' | 'owner' | 'unknown',
+	};
+
+	const isAllowedToView = await verifyUserPermissionLevel(permissionLevelInput, existingUserRank);
+
+	if (!isAllowedToView) {
+		return simpleResponse(401, 'Unauthorized');
+	}
+
+	return new Response(JSON.stringify(data), {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+};
 
 export const OPTIONS: APIRoute = async () => {
 	return new Response(null, {
 		status: 204,
 		statusText: 'No Content',
 		headers: {
-			Allow: 'OPTIONS',
+			Allow: 'OPTIONS, GET',
 			'ALLOW-ACCESS-CONTROL-ORIGIN': '*',
 			'Cache-Control': 'public, max-age=604800, immutable',
 			Date: new Date().toUTCString(),
--- packages/studiocms/src/routes/rest/v1/users/index.ts
+++ packages/studiocms/src/routes/rest/v1/users/index.ts
@@ -1,11 +1,149 @@
-import type { APIRoute } from 'astro';
+import { verifyPasswordStrength } from 'studiocms:auth/lib/password';
+import { createLocalUser, verifyUsernameInput } from 'studiocms:auth/lib/user';
+import studioCMS_SDK from 'studiocms:sdk';
+import type { APIContext, APIRoute } from 'astro';
+import { z } from 'astro/zod';
+import { simpleResponse } from '../../../../utils/simpleResponse.js';
+import { verifyAuthToken } from '../../utils/auth-token.js';
+
+type JSONData = {
+	username: string | undefined;
+	password: string | undefined;
+	email: string | undefined;
+	displayname: string | undefined;
+	rank: 'owner' | 'admin' | 'editor' | 'visitor' | undefined;
+};
+
+export const GET: APIRoute = async (context: APIContext) => {
+	const user = await verifyAuthToken(context);
+
+	if (user instanceof Response) {
+		return user;
+	}
+
+	const { rank } = user;
+
+	if (rank !== 'owner' && rank !== 'admin') {
+		return simpleResponse(401, 'Unauthorized');
+	}
+
+	const users = await studioCMS_SDK.GET.database.users();
+
+	const data = users.map(
+		({ avatar, createdAt, email, id, name, permissionsData, updatedAt, url, username }) => ({
+			avatar,
+			createdAt,
+			email,
+			id,
+			name,
+			rank: permissionsData?.rank ?? 'unknown',
+			updatedAt,
+			url,
+			username,
+		})
+	);
+
+	return new Response(JSON.stringify(data), {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+};
+
+export const POST: APIRoute = async (context: APIContext) => {
+	const user = await verifyAuthToken(context);
+
+	if (user instanceof Response) {
+		return user;
+	}
+
+	const { rank } = user;
+
+	if (rank !== 'owner' && rank !== 'admin') {
+		return simpleResponse(401, 'Unauthorized');
+	}
+
+	const jsonData: JSONData = await context.request.json();
+
+	let { username, password, email, displayname, rank: newUserRank } = jsonData;
+
+	if (!username) {
+		return simpleResponse(400, 'Missing field: Username is required');
+	}
+
+	if (!password) {
+		password = studioCMS_SDK.generateRandomPassword(12);
+	}
+
+	if (!email) {
+		return simpleResponse(400, 'Missing field: Email is required');
+	}
+
+	if (!displayname) {
+		return simpleResponse(400, 'Missing field: Display name is required');
+	}
+
+	if (!newUserRank) {
+		return simpleResponse(400, 'Missing field: Rank is required');
+	}
+
+	// If the username is invalid, return an error
+	if (verifyUsernameInput(username) !== true) {
+		return simpleResponse(
+			400,
+			'Invalid username: Username must be between 3 and 20 characters, only contain lowercase letters, numbers, -, and _ as well as not be a commonly used username (admin, root, etc.)'
+		);
+	}
+
+	// If the password is invalid, return an error
+	if ((await verifyPasswordStrength(password)) !== true) {
+		return simpleResponse(
+			400,
+			'Invalid password: Password must be between 6 and 255 characters, and not be in the <a href="https://haveibeenpwned.com/Passwords" target="_blank">pwned password database</a>.'
+		);
+	}
+
+	// If the email is invalid, return an error
+	const checkEmail = z.coerce
+		.string()
+		.email({ message: 'Email address is invalid' })
+		.safeParse(email);
+
+	if (!checkEmail.success) {
+		return simpleResponse(400, `Invalid email: ${checkEmail.error.message}`);
+	}
+
+	const { usernameSearch, emailSearch } =
+		await studioCMS_SDK.AUTH.user.searchUsersForUsernameOrEmail(username, checkEmail.data);
+
+	if (usernameSearch.length > 0) {
+		return simpleResponse(400, 'Invalid username: Username is already in use');
+	}
+
+	if (emailSearch.length > 0) {
+		return simpleResponse(400, 'Invalid email: Email is already in use');
+	}
+
+	// Create a new user
+	const newUser = await createLocalUser(displayname, username, email, password);
+
+	const updateRank = await studioCMS_SDK.UPDATE.permissions({
+		user: newUser.id,
+		rank: rank,
+	});
+
+	return simpleResponse(
+		200,
+		JSON.stringify({ username, email, displayname, rank: updateRank.rank, password })
+	);
+};
 
 export const OPTIONS: APIRoute = async () => {
 	return new Response(null, {
 		status: 204,
 		statusText: 'No Content',
 		headers: {
-			Allow: 'OPTIONS',
+			Allow: 'OPTIONS, GET, POST',
 			'ALLOW-ACCESS-CONTROL-ORIGIN': '*',
 			'Cache-Control': 'public, max-age=604800, immutable',
 			Date: new Date().toUTCString(),
