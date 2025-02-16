23493047a08d42b86c32727cc487d600c67ab64a
Adam Matthiesen <amatthiesen@outlook.com>
2025-02-16 05:10:18 -0800

    Add PATCH endpoint for updating site configuration with authorization checks

--- packages/studiocms/src/routes/rest/v1/settings/index.ts
+++ packages/studiocms/src/routes/rest/v1/settings/index.ts
@@ -26,12 +26,52 @@ export const GET: APIRoute = async (context: APIContext) => {
 	});
 };
 
+export const PATCH: APIRoute = async (context: APIContext) => {
+	const user = await verifyAuthToken(context);
+
+	if (user instanceof Response) {
+		return user;
+	}
+
+	const { rank } = user;
+
+	if (rank !== 'owner') {
+		return simpleResponse(401, 'Unauthorized');
+	}
+
+	const siteConfig: Omit<tsSiteConfigSelect, 'id'> = await context.request.json();
+
+	if (!siteConfig.title) {
+		return simpleResponse(400, 'Invalid form data, title is required');
+	}
+
+	if (!siteConfig.description) {
+		return simpleResponse(400, 'Invalid form data, description is required');
+	}
+
+	if (!siteConfig.loginPageBackground) {
+		return simpleResponse(400, 'Invalid form data, loginPageBackground is required');
+	}
+
+	if (siteConfig.loginPageBackground === 'custom' && !siteConfig.loginPageCustomImage) {
+		return simpleResponse(400, 'Invalid form data, loginPageCustomImage is required');
+	}
+
+	try {
+		await studioCMS_SDK_Cache.UPDATE.siteConfig(siteConfig);
+
+		return simpleResponse(200, 'Site config updated');
+	} catch (error) {
+		return simpleResponse(500, 'Error updating site config');
+	}
+};
+
 export const OPTIONS: APIRoute = async () => {
 	return new Response(null, {
 		status: 204,
 		statusText: 'No Content',
 		headers: {
-			Allow: 'OPTIONS, GET',
+			Allow: 'OPTIONS, GET, PATCH',
 			'ALLOW-ACCESS-CONTROL-ORIGIN': '*',
 			'Cache-Control': 'public, max-age=604800, immutable',
 			Date: new Date().toUTCString(),
