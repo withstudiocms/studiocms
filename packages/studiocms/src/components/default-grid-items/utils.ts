08545d6e10eed7b169b797fa63a72b3f23a92e02
Adam Matthiesen <amatthiesen@outlook.com>
2025-02-20 01:03:10 -0800

    Refactor date sorting in Recently-created, Recently-signed-up, and Recently-updated components to use sortByDate utility function

--- packages/studiocms/src/components/default-grid-items/utils.ts
+++ packages/studiocms/src/components/default-grid-items/utils.ts
@@ -1,25 +1,42 @@
-
 // Only allow the following identifiers for pages
 export const allowedIdentifiers = ['studiocms/markdown'];
 
+/**
+ * Checks if a given date is within the last 30 days from the current date.
+ *
+ * @param date - The date to check.
+ * @returns True if the date is within the last 30 days, otherwise false.
+ */
 export function withinLast30Days(date: Date) {
-    const now = new Date();
-    const thirtyDaysAgo = new Date(now.setDate(now.getDate() - 30));
-    return date > thirtyDaysAgo;
+	const now = new Date();
+	const thirtyDaysAgo = new Date(now.setDate(now.getDate() - 30));
+	return date > thirtyDaysAgo;
 }
 
-export function sorter(a: Date | null, b: Date | null) {
-    if (!a && !b) {
-        return 0;
-    }
+/**
+ * Sorts two dates in ascending or descending order.
+ *
+ * @param a - The first date to compare. Can be null.
+ * @param b - The second date to compare. Can be null.
+ * @param desc - If true, sorts in descending order. Defaults to ascending order.
+ * @returns A negative number if `a` is less than `b`, zero if they are equal, or a positive number if `a` is greater than `b`.
+ */
+export function sortByDate(a: Date | null, b: Date | null, desc?: boolean) {
+	if (!a && !b) {
+		return 0;
+	}
 
-    if (!a) {
-        a = new Date(0);
-    }
+	if (!a) {
+		a = new Date(0);
+	}
 
-    if (!b) {
-        b = new Date(0);
-    }
+	if (!b) {
+		b = new Date(0);
+	}
 
-    return b.getTime() - a.getTime();
+	if (desc) {
+		return a.getTime() - b.getTime();
+	}
+
+	return b.getTime() - a.getTime();
 }
