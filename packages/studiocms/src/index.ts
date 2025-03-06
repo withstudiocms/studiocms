cbfcea1a0deb46200da474d1ff88b1e488cd2539
Adam Matthiesen <amatthiesen@outlook.com>
2025-03-05 21:57:30 -0800

    fix: streamline apiResponseLogger for improved error handling and logging

--- packages/studiocms/src/index.ts
+++ packages/studiocms/src/index.ts
@@ -1083,38 +1083,28 @@ export const studiocms = defineIntegration({
 								import { logger } from '@it-astro:logger:studiocms-runtime';
 
 								const runtimeLogger = logger.fork('studiocms:runtime');
-
-								export default runtimeLogger;
-
 								const isVerbose = ${verbose};
-
 								function buildErrorMessage(message: string, error?: Error | any) {
 									if (!error) return message;
-
 									if (error instanceof Error) return message + ': ' + error.message + '\n' + error.stack;
-
 									return message + ': ' + error;
-								}
+								};
 
 								export function apiResponseLogger(status: number, message: string, error?: Error | any) {
 									if (status !== 200) {
-
 										isVerbose && runtimeLogger.error(buildErrorMessage(message));
-										return new Response(
-											JSON.stringify({ error: message }), { 
-												status, 
-												headers: { 'Content-Type': 'application/json' } 
-											}
-										);	
-									}
-									
-									return new Response(
-										JSON.stringify({ message }), { 
+										return new Response(JSON.stringify({ error: message }), { 
 											status, 
 											headers: { 'Content-Type': 'application/json' } 
-										}
-									);
-								}
+										});
+									}
+									return new Response(JSON.stringify({ message }), { 
+										status, 
+										headers: { 'Content-Type': 'application/json' } 
+									});
+								};
+
+								export default runtimeLogger;
 							`,
 
 							// Plugin Helpers
