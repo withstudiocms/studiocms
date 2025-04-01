aaab95e80289b6c5369ae9e9735f939853fd705e
Adam Matthiesen <amatthiesen@outlook.com>
2025-04-01 16:43:24 -0700

    fix(cli): correct username validation check in user creation process

--- packages/studiocms/src/cli/cmds/users/steps/libsql/createUsers.ts
+++ packages/studiocms/src/cli/cmds/users/steps/libsql/createUsers.ts
@@ -34,7 +34,7 @@ export async function libsqlCreateUsers(ctx: Context) {
 					validate: (user) => {
 						const isUser = currentUsers.find(({ username }) => username === user);
 						if (isUser) return 'Username is already in use, please try another one';
-						if (checkIfUnsafe(username).username()) {
+						if (checkIfUnsafe(user).username()) {
 							return 'Username should not be a commonly used unsafe username (admin, root, etc.)';
 						}
 						return undefined;
