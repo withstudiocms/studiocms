6e4274803e7bc40a8634728ae84613ebd2b4dd36
Adam Matthiesen <amatthiesen@outlook.com>
2025-05-14 22:28:34 -0700

    feat(middleware): refactor middlewareEffect and getUserPermissions for improved structure and logging

--- packages/studiocms/src/middleware/index.ts
+++ packages/studiocms/src/middleware/index.ts
@@ -1,30 +1,45 @@
 import { defineMiddleware } from 'astro:middleware';
-import { getUserData } from 'studiocms:auth/lib/user';
-import { isEmailVerificationEnabled } from 'studiocms:auth/lib/verify-email';
+import { User } from 'studiocms:auth/lib/user';
+import { VerifyEmail } from 'studiocms:auth/lib/verify-email';
 import { defaultLang } from 'studiocms:i18n';
 import { StudioCMSRoutes } from 'studiocms:lib';
-import studioCMS_SDK_Cache from 'studiocms:sdk/cache';
+import { SDKCore } from 'studiocms:sdk';
 import SCMSUiVersion from 'studiocms:ui/version';
 import SCMSVersion from 'studiocms:version';
+import type { APIContext, MiddlewareNext } from 'astro';
+import { Effect } from 'effect';
+import { convertToVanilla, genLogger } from '../lib/effects/index.js';
 import { getUserPermissions } from './utils.js';
 
-export const onRequest = defineMiddleware(async (context, next) => {
-	const [userSessionData, emailVerificationEnabled, latestVersion, siteConfig] = await Promise.all([
-		getUserData(context),
-		isEmailVerificationEnabled(),
-		studioCMS_SDK_Cache.GET.latestVersion(),
-		studioCMS_SDK_Cache.GET.siteConfig(),
-	]);
+const middlewareEffect = (context: APIContext, next: MiddlewareNext) =>
+	genLogger('studiocms/middleware/middlewareEffect')(function* () {
+		const sdk = yield* SDKCore;
+		const user = yield* User;
+		const email = yield* VerifyEmail;
 
-	context.locals.SCMSGenerator = `StudioCMS v${SCMSVersion}`;
-	context.locals.SCMSUiGenerator = `StudioCMS UI v${SCMSUiVersion}`;
-	context.locals.latestVersion = latestVersion;
-	context.locals.siteConfig = siteConfig;
-	context.locals.userSessionData = userSessionData;
-	context.locals.emailVerificationEnabled = emailVerificationEnabled;
-	context.locals.defaultLang = defaultLang;
-	context.locals.routeMap = StudioCMSRoutes;
-	context.locals.userPermissionLevel = getUserPermissions(userSessionData);
+		const latestVersion = yield* sdk.GET.latestVersion();
+		const siteConfig = yield* sdk.GET.siteConfig();
+		const userSessionData = yield* user.getUserData(context);
+		const emailVerificationEnabled = yield* email.isEmailVerificationEnabled();
+		const userPermissionLevel = yield* getUserPermissions(userSessionData);
 
-	return next();
-});
+		context.locals.SCMSGenerator = `StudioCMS v${SCMSVersion}`;
+		context.locals.SCMSUiGenerator = `StudioCMS UI v${SCMSUiVersion}`;
+		context.locals.latestVersion = latestVersion;
+		context.locals.siteConfig = siteConfig;
+		context.locals.defaultLang = defaultLang;
+		context.locals.routeMap = StudioCMSRoutes;
+		context.locals.userSessionData = userSessionData;
+		context.locals.emailVerificationEnabled = emailVerificationEnabled;
+		context.locals.userPermissionLevel = userPermissionLevel;
+
+		return next();
+	}).pipe(
+		Effect.provide(SDKCore.Default),
+		Effect.provide(User.Default),
+		Effect.provide(VerifyEmail.Default)
+	);
+
+export const onRequest = defineMiddleware(
+	async (context, next) => await convertToVanilla(middlewareEffect(context, next))
+);
