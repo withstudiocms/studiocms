fe97f2aa4e0bcd826470fe40c1ceef75802f45e4
Adam Matthiesen <amatthiesen@outlook.com>
2025-07-21 21:35:01 -0700

    fix siteconfig in sdk

--- packages/studiocms/src/sdk/sdkCore.ts
+++ packages/studiocms/src/sdk/sdkCore.ts
@@ -1,5 +1,5 @@
 import { and, asc, desc, eq } from 'astro:db';
-import { sdk as sdkConfig } from 'studiocms:config';
+import { config, sdk as sdkConfig } from 'studiocms:config';
 import { createTwoFilesPatch } from 'diff';
 import { type Diff2HtmlConfig, html } from 'diff2html';
 import { Effect } from 'effect';
@@ -2429,6 +2429,22 @@ export class SDKCore extends Effect.Service<SDKCore>()('studiocms/sdk/SDKCore', 
 			siteConfig: () =>
 				Effect.gen(function* () {
 					const status = yield* isCacheEnabled;
+
+					if (config.dbStartPage) {
+						return siteConfigReturn({
+							defaultOgImage: null,
+							description: 'A StudioCMS Project',
+							diffPerPage: 10,
+							enableDiffs: false,
+							enableMailer: false,
+							gridItems: [],
+							hideDefaultIndex: false,
+							loginPageBackground: 'studiocms-curves',
+							loginPageCustomImage: null,
+							siteIcon: null,
+							title: 'StudioCMS',
+						});
+					}
 
 					if (!status) {
 						const newConfig = yield* dbService.execute((db) =>
