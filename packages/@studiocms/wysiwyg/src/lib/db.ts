a902e25e6c5d5bd0d9e37ee496ceacc3414c5989
Adam Matthiesen <amatthiesen@outlook.com>
2025-08-06 04:17:58 -0700

    feat: restructure StudioCMSProjectData schema for improved type validation and organization

--- packages/@studiocms/wysiwyg/src/lib/db.ts
+++ packages/@studiocms/wysiwyg/src/lib/db.ts
@@ -4,15 +4,47 @@ import { Effect, Schema } from 'studiocms/effect';
 
 // this is used for testing, and is planned to be used in the future
 
-export interface StudioCMSProjectData extends ProjectData {
-	__STUDIOCMS_HTML?: string;
-}
+const AnyArray = Schema.Array(Schema.Any);
 
-export const effectSchema = Schema.Struct({
+const Component = Schema.Struct({
+	type: Schema.String,
+	stylable: AnyArray,
+	attributes: Schema.Record({ key: Schema.String, value: Schema.Any }),
+	components: AnyArray,
+	head: Schema.Struct({
+		type: Schema.String,
+	}),
+	docEl: Schema.Struct({
+		tagName: Schema.String,
+	}),
+});
+
+const Frame = Schema.Struct({
+	id: Schema.String,
+	component: Component,
+});
+
+const Frames = Schema.Array(Frame);
+
+const Page = Schema.Struct({
+	id: Schema.String,
+	name: Schema.String,
+	frames: Frames,
+});
+
+const Pages = Schema.Array(Page);
+
+export const StudioCMSProjectData = Schema.Struct({
+	dataSources: AnyArray,
+	assets: AnyArray,
+	styles: AnyArray,
+	pages: Pages,
+	symbols: AnyArray,
 	__STUDIOCMS_HTML: Schema.optional(Schema.String),
 });
 
-export type EffectSchema = (typeof effectSchema)['fields'];
+export type StudioCMSProjectData = (typeof StudioCMSProjectData)['Type'] & ProjectData;
+export type StudioCMSProjectDataSchema = (typeof StudioCMSProjectData)['fields'];
 
 export const PLUGIN_ID = 'studiocms-wysiwyg';
 
@@ -22,15 +54,20 @@ export const UseSDK = Effect.gen(function* () {
 	const { usePluginData } = sdk.PLUGINS;
 
 	const _usePluginDataCategory = () =>
-		usePluginData<StudioCMSProjectData, EffectSchema>(PLUGIN_ID, {
-			validator: { effectSchema },
+		usePluginData<StudioCMSProjectData, StudioCMSProjectDataSchema>(PLUGIN_ID, {
+			validator: { effectSchema: StudioCMSProjectData },
 		});
 
 	const _usePluginDataSingle = (id: string) =>
-		usePluginData<StudioCMSProjectData, EffectSchema>(PLUGIN_ID, {
+		usePluginData<StudioCMSProjectData, StudioCMSProjectDataSchema>(PLUGIN_ID, {
 			entryId: id,
-			validator: { effectSchema },
+			validator: { effectSchema: StudioCMSProjectData },
 		});
+
+	const test = yield* _usePluginDataSingle('test').select();
+
+	if (test) {
+	}
 
 	return {
 		getAll: () => _usePluginDataCategory().getEntries(),
