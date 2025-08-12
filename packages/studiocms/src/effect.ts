74dd489a6d3b2ff56fca162235cf5c3eba15c17f
Adam Matthiesen <amatthiesen@outlook.com>
2025-08-12 02:43:14 -0700

    refactor: streamline Effect exports and enhance documentation

--- packages/studiocms/src/effect.ts
+++ packages/studiocms/src/effect.ts
@@ -1,11 +1,7 @@
 // Re-export the Effect package
+// Note: We proxy the Effect package from @withstudiocms/effect to avoid circular dependencies,
+// when importing Effect utilities from different parts of the codebase and packages
 export * from '@withstudiocms/effect';
 
-// Export custom Effect utils
-export {
-	convertToVanilla,
-	errorTap,
-	genLogger,
-	pipeLogger,
-	runtimeLogger,
-} from './lib/effects/index.js';
+// Export custom Effect utils Specific to StudioCMS
+export * from './lib/effects/index.js';
