52dd05e70868c725736b8827f50b6adb60bb34b7
Adam Matthiesen <amatthiesen@outlook.com>
2025-08-20 15:00:03 -0700

    feat: integrate @withstudiocms/component-registry package and update dependencies

---
+++ .changeset/hungry-pandas-spend.md
@@ -0,0 +1,6 @@
+---
+"@studiocms/wysiwyg": patch
+"studiocms": patch
+---
+
+Updates to utilize new `@withstudiocms/component-registry` package
--- packages/studiocms/src/componentRegistry/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './convert-hyphens.js';
-export * from './errors.js';
-export * from './handler.js';
-export * from './PropsParser.js';
-export * from './Registry.js';
-export * from './types.js';
