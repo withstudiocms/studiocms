6abe6063b44471a4ccab63cb70b4bc2db5d92b28
Adam Matthiesen <amatthiesen@outlook.com>
2025-08-20 04:04:24 -0700

    refactor(date-time): unify date-time configuration for last updated checks

--- packages/studiocms/src/components/dashboard/component-scripts/dateTimeListener.ts
+++ packages/studiocms/src/components/dashboard/component-scripts/dateTimeListener.ts
@@ -1,12 +1,10 @@
+import { DTConfig } from './dateWithTimeAndZone.js';
+
 export function dateTimeListener(id: string) {
 	const lastCheckedDate = document.getElementById(id) as HTMLTimeElement;
 
-	lastCheckedDate.textContent = new Date(lastCheckedDate.dateTime).toLocaleString(undefined, {
-		month: 'short',
-		day: 'numeric',
-		year: 'numeric',
-		hour: 'numeric',
-		minute: 'numeric',
-		timeZoneName: 'short',
-	});
+	lastCheckedDate.textContent = new Date(lastCheckedDate.dateTime).toLocaleString(
+		undefined,
+		DTConfig
+	);
 }
--- packages/studiocms/src/components/dashboard/component-scripts/dateWithTimeAndZone.ts
+++ packages/studiocms/src/components/dashboard/component-scripts/dateWithTimeAndZone.ts
@@ -1,9 +1,12 @@
+export const DTConfig: Intl.DateTimeFormatOptions = {
+	month: 'short',
+	day: 'numeric',
+	year: 'numeric',
+	hour: 'numeric',
+	minute: 'numeric',
+	timeZoneName: 'short',
+};
+
 export function dateWithTimeAndZone(date: Date) {
-	return date.toLocaleString(undefined, {
-		month: 'numeric',
-		day: 'numeric',
-		year: 'numeric',
-		hour: 'numeric',
-		minute: 'numeric',
-	});
+	return date.toLocaleString(undefined, DTConfig);
 }
