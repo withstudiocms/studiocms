290402981c3d044d554048b7f57b2ca09750047b
Adam Matthiesen <amatthiesen@outlook.com>
2025-08-21 17:21:01 -0700

    feat: move ThemeManager to shared directory and update imports across components

--- packages/studiocms/src/components/dashboard/component-scripts/makeClientRoutable.ts
+++
@@ -1,7 +0,0 @@
-// biome-ignore lint/suspicious/noExplicitAny: this is a valid use case for explicit any.
-export function makeClientRoutable(func: any) {
-	func;
-	document.addEventListener('astro:page-load', () => {
-		func;
-	});
-}
