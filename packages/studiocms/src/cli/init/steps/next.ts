1086b01dee1e522299d5afd8b7f5debdf7269ada
Adam Matthiesen <amatthiesen@outlook.com>
2025-09-08 01:28:16 -0700

    refactor: consolidate logging and success messages in next step function for improved clarity

--- packages/studiocms/src/cli/init/steps/next.ts
+++ packages/studiocms/src/cli/init/steps/next.ts
@@ -5,7 +5,7 @@ import {
 } from '@withstudiocms/cli-kit/colors';
 import { boxen, label } from '@withstudiocms/cli-kit/messages';
 import { log, outro } from '@withstudiocms/effect/clack';
-import { genLogger } from '../../../effect.js';
+import { Effect, genLogger } from '../../../effect.js';
 import { CliContext } from '../../utils/context.js';
 import { buildDebugLogger } from '../../utils/logger.js';
 
@@ -18,32 +18,34 @@ const commandMap: { [key: string]: string } = {
 
 export const next = (debug: boolean) =>
 	genLogger('studiocms/cli/init/steps/next')(function* () {
-		const { chalk, packageManager } = yield* CliContext;
-
-		const debugLogger = yield* buildDebugLogger(debug);
+		const [{ chalk, packageManager }, debugLogger] = yield* Effect.all([
+			CliContext,
+			buildDebugLogger(debug),
+		]);
 
 		const devCmd = commandMap[packageManager as keyof typeof commandMap] || 'npm run dev';
 
-		yield* debugLogger(`Dev command: ${devCmd}`);
+		yield* Effect.all([
+			debugLogger(`Dev command: ${devCmd}`),
+			debugLogger('Running next steps fn...'),
+			log.success(
+				boxen(
+					chalk.bold(
+						`${label('Init Complete!', StudioCMSColorwayInfoBg, chalk.bold)} Get started with StudioCMS:`
+					),
+					{
+						ln1: `Ensure your ${chalk.cyanBright('.env')} file is configured correctly.`,
+						ln3: `Run ${chalk.cyan('astro db push')} to sync your database schema.`,
+						ln4: `Run ${chalk.cyan(devCmd)} to start the dev server. ${chalk.cyanBright('CTRL+C')} to stop.`,
+					}
+				)
+			),
+		]);
 
-		yield* debugLogger('Running next steps fn...');
-
-		yield* log.success(
-			boxen(
-				chalk.bold(
-					`${label('Init Complete!', StudioCMSColorwayInfoBg, chalk.bold)} Get started with StudioCMS:`
-				),
-				{
-					ln1: `Ensure your ${chalk.cyanBright('.env')} file is configured correctly.`,
-					ln3: `Run ${chalk.cyan('astro db push')} to sync your database schema.`,
-					ln4: `Run ${chalk.cyan(devCmd)} to start the dev server. ${chalk.cyanBright('CTRL+C')} to stop.`,
-				}
-			)
-		);
-
-		yield* outro(
-			`${label('Enjoy your new CMS!', StudioCMSColorwayBg, chalk.bold)} Stuck? Join us on Discord at ${StudioCMSColorway.bold.underline('https://chat.studiocms.dev')}`
-		);
-
-		yield* debugLogger('Next steps complete');
+		yield* Effect.all([
+			outro(
+				`${label('Enjoy your new CMS!', StudioCMSColorwayBg, chalk.bold)} Stuck? Join us on Discord at ${StudioCMSColorway.bold.underline('https://chat.studiocms.dev')}`
+			),
+			debugLogger('Next steps complete'),
+		]);
 	});
