1831b38d10e4e8eb98e084c29db1fa0291f8f8bb
Adam Matthiesen <amatthiesen@outlook.com>
2025-09-10 15:18:51 -0700

    fix(ci): update Codecov slug for correct reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		projects: ['packages/*', 'packages/@withstudiocms/*', 'packages/@studiocms/*'],
-		reporters: ['default', 'junit'],
-		outputFile: {
-			junit: './junit-report.xml',
-		},
+		// reporters: ['default', 'junit'],
+		// outputFile: {
+		// 	junit: './junit-report.xml',
+		// },
 		coverage: {
 			provider: 'v8',
+			reporter: ['text', 'json'],
 		},
 	},
 });
