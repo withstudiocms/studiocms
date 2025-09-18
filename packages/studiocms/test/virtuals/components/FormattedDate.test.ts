d6d3339e25755b46ec1ed4657883a9dc09a3dd5d
Adam Matthiesen <amatthiesen@outlook.com>
2025-09-17 23:12:40 -0700

    feat(FormattedDate): add __test_mode prop for UTC date formatting and update tests

--- packages/studiocms/test/virtuals/components/FormattedDate.test.ts
+++ packages/studiocms/test/virtuals/components/FormattedDate.test.ts
@@ -8,7 +8,7 @@ describe('FormattedDate component', () => {
 	test('render component', async () => {
 		const container = await AstroContainer.create();
 		const result = await container.renderToString(FormattedDate, {
-			props: { date: new Date('2020-01-01 01:00:00') },
+			props: { date: new Date('2020-01-01'), __test_mode: true },
 		});
 		const cleanResult = cleanAstroAttributes(result, '/mock/path/FormattedDate.astro');
 		expect(cleanResult).toMatchSnapshot();
