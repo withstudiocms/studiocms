6abc42b2a8f0c42ad2896c627360fde8cfb5d8a1
Adam Matthiesen <amatthiesen@outlook.com>
2025-10-07 12:17:20 -0700

    init new package

---
+++ packages/@withstudiocms/template-lang/src/index.ts
@@ -0,0 +1,10 @@
+// Main exports
+export { TemplateEngine } from './engine.js';
+export { TemplateParser } from './parser.js';
+export { TemplateRenderer } from './renderer.js';
+
+// Type exports
+export type { TemplateData, TemplateOptions, TemplateVariable } from './types.js';
+
+// Default export for convenience
+export { TemplateEngine as default } from './engine.js';
