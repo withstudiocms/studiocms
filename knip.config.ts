f7a7d9026bb519fefa86d2754abe74f1ccbb8a87
Adam Matthiesen <amatthiesen@outlook.com>
2025-10-22 03:57:03 -0700

    fix: Update CI configuration to ignore specific binaries for matrix builds

--- knip.config.ts
+++ knip.config.ts
@@ -166,6 +166,8 @@ const config: KnipConfig = {
 	workspaces: {
 		'.': {
 			ignoreDependencies: ['@changesets/config', 'studiocms', 'vite', 'allure-js-commons'],
+			// biome-ignore lint/suspicious/noTemplateCurlyInString: This is intended for matrix builds
+			ignoreBinaries: ['ci:typecheck:${{ matrix.cmd }}', 'bundle-test:${{ matrix.cmd }}'],
 			entry: ['.github/workflows/*.yml', '.github/scripts/**/*.mjs', 'scripts/**/*.mjs'],
 			project: ['.github/scripts/**/*.mjs', 'scripts/**/*.mjs'],
 		},
