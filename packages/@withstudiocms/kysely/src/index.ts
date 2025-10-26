02a54cd98190bf5c4829b44512d95b6b8d4a6011
Adam Matthiesen <amatthiesen@outlook.com>
2025-10-26 04:25:45 -0700

    feat: add schema exports to package and update index for improved module structure

--- packages/@withstudiocms/kysely/src/index.ts
+++ packages/@withstudiocms/kysely/src/index.ts
@@ -1,2 +1,8 @@
-export * from './client.js';
+/**
+ * Migrator is left out of this index to avoid issues with `node:` imports in ESM/CJS
+ * environments that do not support them. Users should import migrator-related
+ * functions directly from the migrator module. `@withstudiocms/kysely/migrator`.
+ */
+export * from './core/schema.js';
+export * from './schema.js';
 export * from './tables.js';
