2a42b1bca2ea295635e50fcc57fb280dc8256cff
Adam Matthiesen <amatthiesen@outlook.com>
2025-10-29 15:13:05 -0700

    feat: add GetVersionFromNPM utility for retrieving NPM package versions

---
+++ packages/@withstudiocms/sdk/src/modules/util/getVersionFromNPM.ts
@@ -0,0 +1,49 @@
+import { Effect, HTTPClient, Platform, Schema } from '@withstudiocms/effect';
+
+/**
+ * Represents the version information retrieved from NPM.
+ *
+ * @extends Schema.Class<NpmVersion>
+ * @property version - The version string of the NPM package.
+ */
+export class NpmVersion extends Schema.Class<NpmVersion>('NpmVersion')({
+	version: Schema.String,
+}) {}
+
+/**
+ * An Effect service for retrieving the version of an NPM package from the NPM registry.
+ *
+ * @remarks
+ * This service uses an HTTP client with retry logic to fetch the version information
+ * for a given package and version (defaulting to 'latest') from the NPM registry.
+ *
+ * @example
+ * ```typescript
+ * const version = await GetVersionFromNPM.get('react');
+ * ```
+ *
+ * @method get
+ * @param pkg - The name of the NPM package.
+ * @param ver - The version tag or number (defaults to 'latest').
+ * @returns An Effect that resolves to the version string of the specified package.
+ */
+export const GetVersionFromNPM = Effect.gen(function* () {
+	const client = yield* HTTPClient;
+
+	/**
+	 * Retrieves the version of an NPM package.
+	 *
+	 * @param pkg - The name of the NPM package.
+	 * @param ver - The version tag or number (defaults to 'latest').
+	 * @returns An Effect that resolves to the version string of the specified package.
+	 */
+	const get = (pkg: string, ver = 'latest') =>
+		Effect.gen(function* () {
+			const response = yield* client
+				.get(`https://registry.npmjs.org/${pkg}/${ver}`)
+				.pipe(Effect.flatMap(Platform.HttpClientResponse.schemaBodyJson(NpmVersion)));
+			return response.version;
+		});
+
+	return { get };
+}).pipe(Effect.provide(HTTPClient.Default));
--- packages/@withstudiocms/sdk/src/modules/util/index.ts
+++ packages/@withstudiocms/sdk/src/modules/util/index.ts
@@ -1,6 +1,8 @@
 import { Effect } from '@withstudiocms/effect';
 import { SDKGenerators as Generators } from './generators.js';
+import { GetVersionFromNPM } from './getVersionFromNPM.js';
 
 export const SDKUtilModule = Effect.all({
 	Generators,
+	GetVersionFromNPM,
 });
