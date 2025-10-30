4d6302ed07a390e3af95d7d8297368a7893f61a7
Adam Matthiesen <amatthiesen@outlook.com>
2025-10-30 05:49:31 -0700

    feat: enhance SDK utility module by adding placeholders for Collectors, Parsers, and Users

--- packages/@withstudiocms/sdk/src/modules/util/index.ts
+++ packages/@withstudiocms/sdk/src/modules/util/index.ts
@@ -3,8 +3,18 @@ import { SDKFolderTree as FolderTree } from './folderTree.js';
 import { SDKGenerators as Generators } from './generators.js';
 import { GetVersionFromNPM } from './getVersionFromNPM.js';
 
+const placeholder = Effect.succeed({});
+
+/**
+ * SDK Util Module
+ *
+ * This module aggregates various utility functionalities of the SDK.
+ */
 export const SDKUtilModule = Effect.all({
+	Collectors: placeholder,
 	FolderTree,
 	Generators,
 	GetVersionFromNPM,
+	Parsers: placeholder,
+	Users: placeholder,
 });
