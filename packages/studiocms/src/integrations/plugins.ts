095e1ebb2a2f0fbac30eb7dd31a03e61a7269804
Adam Matthiesen <amatthiesen@outlook.com>
2025-11-26 21:20:10 -0800

    Move previous webvitals plugin logic to archive until we can rebuild it to it's own plugin for StudioCMS and the new Kysely DB interface

--- packages/studiocms/src/integrations/plugins.ts
+++ packages/studiocms/src/integrations/plugins.ts
@@ -2,5 +2,4 @@
 export * from './dynamic-sitemap/index.js';
 export { default as robotsTXT } from './robots/index.js';
 export * from './robots/schema.js';
-export * from './webVitals/checkForWebVitalsPlugin.js';
 /* v8 ignore stop */
