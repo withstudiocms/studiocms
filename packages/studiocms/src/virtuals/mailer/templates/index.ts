c24b655e74a53fbf0b7a5a47f9d65270f0c3defc
Adam Matthiesen <amatthiesen@outlook.com>
2025-12-05 13:46:27 -0800

    cleanup(mailer): remove deprecated mailer templates and related code (#1050)

--- packages/studiocms/src/virtuals/mailer/templates/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as notification } from './notification.js';
-export { default as passwordReset } from './password-reset.js';
-export { default as userInvite } from './user-invite.js';
-export { default as verifyEmail } from './verify-email.js';
