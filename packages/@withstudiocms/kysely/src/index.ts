77aaa7eb71a98b30e68f0c5989364c7ee9a86925
Adam Matthiesen <amatthiesen@outlook.com>
2026-01-06 00:13:33 -0800

    chore: add TODO for creating Astro integration with Kysely support

--- packages/@withstudiocms/kysely/src/index.ts
+++ packages/@withstudiocms/kysely/src/index.ts
@@ -8,3 +8,9 @@ export * from './client.js';
 export * from './core/schema.js';
 export * from './schema.js';
 export * from './tables.js';
+
+// TODO: Create a new Astro integration to handle an Astro-DB like setup
+// but utilizing Kysely under the hood. This will allow users to also
+// benefit from our new Kysely DB package when using Astro.
+// Astro plans to deprecate Astro-DB in the future, so we should attempt
+// to provide a smooth migration path for our users before that happens.
