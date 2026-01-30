9a276b2631db191321c240bf249354bb2114e308
Adam Matthiesen <amatthiesen@outlook.com>
2026-01-29 16:10:42 -0800

    refactor(api): move buildPartialSchema utility to internal directory for better organization

--- packages/@withstudiocms/api-spec/src/rest-api/schemas.ts
+++ packages/@withstudiocms/api-spec/src/rest-api/schemas.ts
@@ -8,7 +8,7 @@ import {
 	StudioCMSUsersTable,
 } from '@withstudiocms/sdk/tables';
 import { Schema } from 'effect';
-import { buildPartialSchema } from '../_utils/build-partial-schema.js';
+import { buildPartialSchema } from '../_internal/build-partial-schema.js';
 
 /**
  * Select schema for category data.
