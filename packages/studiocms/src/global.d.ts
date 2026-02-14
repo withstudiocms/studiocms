1580b950e510b2feffb0fa42001429ae3ac7b3e2
Adam Matthiesen <amatthiesen@outlook.com>
2026-02-14 11:28:11 -0800

    refactor: remove unused Next plugin hooks interfaces from global.d.ts

--- packages/studiocms/src/global.d.ts
+++ packages/studiocms/src/global.d.ts
@@ -1,18 +1,9 @@
 import type { BasePluginHooks, StorageManagerPluginHooks } from './schemas/plugins';
-import type {
-	BasePluginHooks as Next_BasePluginHooks,
-	StorageManagerPluginHooks as Next_StorageManagerPluginHooks,
-} from './schemas/plugins/index.effect';
 
 // The interfaces in this file can be extended by users
 declare global {
 	namespace StudioCMS {
 		export interface PluginHooks extends BasePluginHooks {}
 		export interface StorageManagerHooks extends BasePluginHooks, StorageManagerPluginHooks {}
-
-		export interface Next_PluginHooks extends Next_BasePluginHooks {}
-		export interface Next_StorageManagerHooks
-			extends Next_BasePluginHooks,
-				Next_StorageManagerPluginHooks {}
 	}
 }
