3397e9f7b3fe1535848543bd040ea796cfca2846
Adam Matthiesen <amatthiesen@outlook.com>
2026-02-21 20:26:48 -0800

    feat(package): add publint dependency and update pnpm-lock.yaml

--- packages/@studiocms/markdown-remark/tsdown.config.ts
+++ packages/@studiocms/markdown-remark/tsdown.config.ts
@@ -6,4 +6,5 @@ export default defineConfig({
 	unbundle: true,
 	entry: 'src/**/**.ts',
 	copy: [{ from: 'src/styles/**/*.css', to: 'dist', flatten: false }],
+	inlineOnly: ['@types/hast', '@types/mdast', '@types/unist'],
 });
