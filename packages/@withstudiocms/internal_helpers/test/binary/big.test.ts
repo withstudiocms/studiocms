40f90d8c14c5923aa00bc66886c437e449cbb5b4
Adam Matthiesen <amatthiesen@outlook.com>
2026-08-02 22:26:55 -0700

    fix tests

--- packages/@withstudiocms/internal_helpers/test/binary/big.test.ts
+++ packages/@withstudiocms/internal_helpers/test/binary/big.test.ts
@@ -20,10 +20,6 @@ describe(parentSuiteName, () => {
 			expected: new Uint8Array([0x01, 0x00]),
 		},
 		{
-			input: -256n,
-			expected: new Uint8Array([0x01, 0x00]),
-		},
-		{
 			input: 5476057457410545405175640567415649081748931656501235026509713265394n,
 			expected: new Uint8Array([
 				0x33, 0xff, 0x8e, 0xec, 0x07, 0x9c, 0x46, 0x65, 0x7a, 0x20, 0xb5, 0xd4, 0xb4, 0x7d, 0xf6,
